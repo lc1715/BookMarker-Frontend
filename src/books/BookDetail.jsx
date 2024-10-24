babd53ed2f09bf1d80a0b8209bc29b78bbc28953
Lisa Chan <lc131715@gmail.com>
2024-10-23 20:12:59 -0700

    add rating component

--- src/books/BookDetail.jsx
+++ src/books/BookDetail.jsx
@@ -1,23 +1,23 @@
-import { useState, useEffect } from 'react';
-import { Navigate, useNavigate, useParams } from 'react-router-dom';
+import { useState, useEffect, useContext } from 'react';
+import { useParams } from 'react-router-dom';
 import BookMarkerApi from '../api/api';
-import LoadSpinner from '../common/LoadSpinner';
-import { useContext } from 'react';
 import UserContext from '../auth/UserContext';
-import ShowAlert from '../common/ShowAlert';
+import LoadSpinner from '../common/LoadSpinner';
 import LoginMessage from '../common/LoginMessage';
 import Review from '../reviews/Review';
 import ReviewForm from '../reviews/ReviewForm';
 import AllReviews from '../reviews/AllReviews';
-
+import Rating from '../rating/Rating';
 
 function BookDetail() {
     const [book, setBook] = useState(null);
     console.log('book=', book)
 
+    const [bookStatus, setBookStatus] = useState(null);
+    console.log('bookStatus=', bookStatus)
+
     const [error, setError] = useState(false);
     console.log('error=', error)
-
     const [allReviews, setAllReviews] = useState([]);
     console.log('allReviews state=', allReviews)
 
@@ -27,9 +27,18 @@ function BookDetail() {
     const [reviewChange, setReviewChange] = useState(false);
     console.log('reviewChange state=', reviewChange)
 
-    const [showReviewForm, setShowReviewForm] = useState(false);
+    const [reviewForm, setReviewForm] = useState(false);
 
     const [showAllReviews, setShowAllReviews] = useState(false);
+
+    const [rating, setRating] = useState(0);
+    console.log('rating state=', rating)
+
+    const [ratingId, setRatingId] = useState(null);
+    console.log('ratingId state=', ratingId)
+
+    const [hover, setHover] = useState(null);
+    console.log('hover state=', hover)
 
     const { currentUser, hasSavedBook, saveBook, deleteSavedBook } = useContext(UserContext);
     console.log('currentUser in BookDetail=', currentUser)
@@ -38,9 +47,10 @@ function BookDetail() {
     //get url params from BookCard. {isbn, 234234} or {volumeId, e783hf39}
     console.log('bookId=', bookId, 'bookIdType=', bookIdType)
 
-    const navigate = useNavigate();
 
-
+    //get book details
+    //if bookId is isbn, call API getGoogleBookFromNYT method to get book detail
+    //if bookId is volumeId, call API getGoogleBook method to get book detail
     //both API methods will return a Google Book with volumeId
     useEffect(() => {
         async function getBookDetails() {
@@ -58,12 +68,30 @@ function BookDetail() {
         getBookDetails()
     }, [bookId, bookIdType]);
 
-    //shows the user's review and option to edit or delete review
+    //get book status to label book as Read or Wish To Read status
     useEffect(() => {
         if (currentUser && book) {
             try {
+                async function getBookStatus() {
+                    let status = await BookMarkerApi.getSavedBook(book.volumeId, currentUser.username);
+                    console.log('book status=', status);
+
+                    if (status) setBookStatus(status.has_read);
+                }
+
+                getBookStatus();
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    }, [book])
+
+    //get all reviews and user's review on the book  
+    useEffect(() => {
+        if (book) {
+            try {
                 async function getAllReviewsAndUserReview() {
-                    //get all reviews 
+                    //get all reviews
                     let reviews = await BookMarkerApi.getAllReviews(book.volumeId);
                     console.log('reviews=', reviews)
 
@@ -75,10 +103,12 @@ function BookDetail() {
                     }
 
                     //get user's review
-                    const review = reviews.filter((review) => (review.username === currentUser.username))
-                    console.log('user review=', review)
+                    if (currentUser) {
+                        let review = reviews.filter((review) => (review.username === currentUser.username))
+                        console.log('user review=', review)
 
-                    review.length ? setReview(review[0]) : setReview(null);
+                        review.length ? setReview(review[0]) : setReview(null);
+                    }
 
                     setAllReviews(reviews);
                     setReviewChange(false);
@@ -90,26 +120,79 @@ function BookDetail() {
         }
     }, [book, reviewChange]);
 
+    //get rating on a book 
+    useEffect(() => {
+        if (currentUser && book) {
+            try {
+                async function showRating() {
+                    let res = await BookMarkerApi.getRating(book.volumeId, currentUser.username);
+
+                    if (res) {
+                        setRating(res.rating)
+                        setRatingId(res.id)
+                    }
+                }
+
+                showRating();
+            } catch (err) {
+                console.log(err);
+            }
+        }
+    }, [book]);
+
+    //add rating on a book
+    async function addRating(star) {
+        if (currentUser) {
+            try {
+                let res = await BookMarkerApi.addRating(book.volumeId, currentUser.username, { rating: star });
+
+                setRating(star);
+                setRatingId(res.id);
+            } catch (err) {
+                console.log(err);
+            }
+        } else {
+            setError(true);
+        }
+    }
+
+    //update rating on a book
+    async function updateRating(star) {
+        if (currentUser) {
+            try {
+                let res = await BookMarkerApi.updateRating(ratingId, currentUser.username, { rating: star });
+
+                setRating(star);
+                setRatingId(res.id);
+            } catch (err) {
+                console.log(err);
+            }
+        } else {
+            setError(true);
+        }
+    }
+
     //add a book to saved books or update status
     async function addBookOrChangeBookStatus(evt) {
         if (currentUser) {
-            //check if has_read value is true or false
+            //check if user's has_read value is true or false
             let has_read_value;
 
             evt.target.innerText === 'Read' ? has_read_value = true : has_read_value = false;
 
             //if book has not been saved, add book. otherwise update has_read status.
             if (!hasSavedBook(book.volumeId)) {
-                await addBook(has_read_value);
-                //navigate to the read or wish to read book list or alert?
+                let res = await addBook(has_read_value);
+
+                setBookStatus(res.has_read);
             } else {
                 try {
-                    await BookMarkerApi.changeBookStatus(
+                    let res = await BookMarkerApi.changeBookStatus(
                         book.volumeId,
                         currentUser.username,
                         { has_read: has_read_value });
 
-                    //navigate to the read or wish to read book list or alert?
+                    setBookStatus(res.has_read);
                 } catch (err) {
                     console.log(err);
                 }
@@ -119,29 +202,28 @@ function BookDetail() {
         }
     };
 
-    //helper func used to add book
+    //Helper function used to add book. Used by BookDetail and ReviewForm.
     async function addBook(has_read_value) {
-        try {
-            await saveBook(book.volumeId, {
-                volume_id: book.volumeId,
-                title: book.title,
-                author: book.author,
-                publisher: book.publisher,
-                category: book.category,
-                description: book.description,
-                image: book.image,
-                has_read: has_read_value
-            });
-        } catch (err) {
-            console.log(err);
-        }
+        let savedBook = await saveBook(book.volumeId, {
+            volume_id: book.volumeId,
+            title: book.title,
+            author: book.author,
+            publisher: book.publisher,
+            category: book.category,
+            description: book.description,
+            image: book.image,
+            has_read: has_read_value
+        });
+
+        return savedBook;
     }
 
-    //delete the saved book in db
+    //delete user's saved book, review and rating
     async function removeSavedBook() {
         try {
             //delete book
             await deleteSavedBook(book.volumeId, currentUser.username);
+            setBookStatus(null);
 
             //delete review
             if (review) {
@@ -150,19 +232,24 @@ function BookDetail() {
                 setReviewChange(true);
             };
 
-            //call delete saved book func in app
-            //delete review here
-            //delete rating here
+            //delete rating
+            if (rating) {
+                await BookMarkerApi.deleteRating(ratingId, currentUser.username);
+
+                setRating(0);
+                setRatingId(null);
+            }
         } catch (err) {
             console.log(err);
         }
     };
 
-    function reviewForm() {
-        currentUser ? setShowReviewForm(true) : setError(true);
+    //show the review form or message to login
+    function showReviewForm() {
+        currentUser ? setReviewForm(true) : setError(true);
     };
 
-    //show reviews comp
+    //show all reviews for a book
     async function showReviews() {
         setShowAllReviews(true)
     };
@@ -171,6 +258,10 @@ function BookDetail() {
 
     return (
         <div>
+
+            {bookStatus === true ? <h3>Read:</h3> : null}
+            {bookStatus === false ? <h3>Wish To Read:</h3> : null}
+
             <img src={book.image} />
             <p>Title: {book.title}</p>
             <p>Author: {book.author}</p>
@@ -184,18 +275,28 @@ function BookDetail() {
 
             <button onClick={showReviews}>All Reviews</button>
 
-            {hasSavedBook(book.volumeId)
+
+            {currentUser && hasSavedBook(book.volumeId)
                 ?
                 <button onClick={removeSavedBook}>Delete Book</button>
                 :
                 null
             }
 
+            <Rating rating={rating} setRating={setRating} addRating={addRating} updateRating={updateRating} hover={hover} setHover={setHover} />
+
             {review
                 ?
-                <Review book={book} userReview={review} setReviewChange={setReviewChange} />
+                <Review userReview={review} setReviewChange={setReviewChange} />
                 :
-                <button onClick={reviewForm}>Write A Review</button>
+                <button onClick={showReviewForm}>Write A Review</button>
+            }
+
+            {reviewForm
+                ?
+                <ReviewForm book={book} addBook={addBook} setReviewChange={setReviewChange} setReviewForm={setReviewForm} setBookStatus={setBookStatus} />
+                :
+                null
             }
 
             {showAllReviews
@@ -205,18 +306,12 @@ function BookDetail() {
                 null
             }
 
-            {showReviewForm
-                ?
-                <ReviewForm book={book} addBook={addBook} setReviewChange={setReviewChange} setShowReviewForm={setShowReviewForm} />
-                :
-                null
-            }
-
             {error ? <LoginMessage setError={setError} /> : null}
         </div>
     )
 };
 
+export default BookDetail;
 
 function removeHtmlTags(string) {
     let doc = new DOMParser().parseFromString(string, 'text/html');
@@ -224,7 +319,7 @@ function removeHtmlTags(string) {
 };
 
 
-export default BookDetail;
+
 
 
 
---
+++ src/rating/Rating.jsx
@@ -0,0 +1,82 @@
+import { useContext } from 'react';
+import UserContext from '../auth/UserContext';
+
+
+function Rating({ rating, setRating, addRating, updateRating, hover, setHover }) {
+    console.log('rating=', rating)
+
+    const { currentUser } = useContext(UserContext);
+
+
+    /* click on star, update rating if rating already exists otherwise add new rating*/
+    async function submitRating(star) {
+        console.log('star=', star)
+
+        if (!rating) {
+            await addRating(star);
+        } else {
+            await updateRating(star)
+        }
+
+        setRating(star);
+    };
+
+
+    function loggedInRating() {
+        return (
+            <div>
+                {
+                    [1, 2, 3, 4, 5].map((star) => {
+                        return (
+                            <span
+                                style={{
+                                    color: star <= (hover || rating) ? 'gold' : 'gray',
+                                    cursor: 'pointer',
+                                    fontSize: '35px'
+                                }}
+                                onClick={() => (submitRating(star))}
+                                onMouseEnter={() => setHover(star)}
+                                onMouseLeave={() => setHover(null)}
+                                key={star}
+                            >
+                                {' '}★{' '}
+                            </span>
+                        )
+                    })
+                }
+            </div>
+        )
+    };
+
+    function loggedOutRating() {
+        return (
+            <div>
+                {
+                    [1, 2, 3, 4, 5].map((star) => {
+                        return (
+                            <span
+                                style={{
+                                    color: 'gray',
+                                    cursor: 'pointer',
+                                    fontSize: '35px'
+                                }}
+                                onClick={() => (submitRating(star))}
+                                key={star}
+                            >
+                                {' '}★{' '}
+                            </span>
+                        )
+                    })
+                }
+            </div>
+        )
+    };
+
+    return (
+        <>
+            {currentUser ? loggedInRating() : loggedOutRating()}
+        </>
+    )
+};
+
+export default Rating;
