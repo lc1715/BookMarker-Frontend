dad0640ed52a466c177c9e818eb26ad69855e566
Lisa Chan <lc131715@gmail.com>
2024-11-10 11:37:06 -0800

    update viewport height

--- src/common/LoadSpinner.jsx
+++ src/common/LoadSpinner.jsx
@@ -6,8 +6,8 @@ import Typography from '@mui/material/Typography';
 
 function LoadSpinner() {
     return (
-        <Box sx={{ width: '100%', height: '53vh', display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>
-            {/* <Box sx={{ width: '100%', height: '75vh'}}> */}
+        <Box sx={{ width: '100%', height: { xs: '63vh', lg: '75vh' }, display: 'flex', justifyContent: 'center', alignItems: 'center', textAlign: 'center' }}>
+            {/* <Box sx={{ width: '100%', height: '75vh' }}> */}
             <Typography sx={{ color: 'text.secondary', fontSize: '28px' }}>
                 <i class="fa-solid fa-spinner fa-spin-pulse fa-lg"></i>
                 <br />
